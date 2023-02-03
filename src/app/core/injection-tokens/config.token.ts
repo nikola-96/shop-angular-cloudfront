eda8146af35737c1fdf481a5dbc51129b56c3c87
nmarkovic-conga <nmarkovic@conga.com>
2023-02-03 14:48:49 +0100

    deleted 2 products

--- src/app/core/injection-tokens/config.token.ts
+++ src/app/core/injection-tokens/config.token.ts
@@ -1,4 +1,4 @@
 import { InjectionToken } from '@angular/core';
 import { Config } from '../../../environments/config.interface';
-
+console.log(new InjectionToken<Config>('CONFIG_TOKEN'));
 export const CONFIG_TOKEN = new InjectionToken<Config>('CONFIG_TOKEN');
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://ok1mira1p2.execute-api.eu-west-1.amazonaws.com/prod',
     order: 'http://localhost:3000/orders',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
